5224711034eb756a48e14a4d0d3165d88750d370
tonnymed12 <anthony.akmp99@gmail.com>
2025-08-22 16:31:38 -0600

    Se agrego la internacionalizacion i18n, con su controol

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,22 +1,22 @@
 sap.ui.define([
-    "sap/ui/core/UIComponent",
-    "sap/ui/model/json/JSONModel"
+	"sap/ui/core/UIComponent",
+	"sap/ui/model/json/JSONModel"
 ], (UIComponent, JSONModel) => {
-    "use strict";
+	"use strict";
 
-    return UIComponent.extend("ui5.databinding.Component", {
-        metadata: {
-            interfaces: ["sap.ui.core.IAsyncContentCreation"],
-            manifest: "json"
-        },
+	return UIComponent.extend("ui5.databinding.Component", {
+		metadata: {
+			interfaces: ["sap.ui.core.IAsyncContentCreation"],
+			manifest: "json"
+		},
 
-        init: function () {
-            UIComponent.prototype.init.apply(this, arguments);
+		init: function () {
+			UIComponent.prototype.init.apply(this, arguments);
 
-            // Cargar y asignar el modelo JSON
-            const oModel = new JSONModel();
-            oModel.loadData("model/data.json");
-            this.setModel(oModel);
-        }
-    });
+			// Cargar y asignar el modelo JSON
+			const oModel = new JSONModel();
+			oModel.loadData("model/data.json", null, false); // <--- carga síncrona
+			this.setModel(oModel);
+		}
+	});
 });
