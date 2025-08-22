882078f6984fc253f46c5e03e8983d86a617150e
tonnymed12 <anthony.akmp99@gmail.com>
2025-08-22 14:48:45 -0600

    Se agrego cargar el modelo en el JS para poder acceder a los datos, y se modifico la vista

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,12 +1,22 @@
 sap.ui.define([
-	"sap/ui/core/UIComponent"
-], (UIComponent) => {
-	"use strict";
+    "sap/ui/core/UIComponent",
+    "sap/ui/model/json/JSONModel"
+], (UIComponent, JSONModel) => {
+    "use strict";
 
-	return UIComponent.extend("ui5.databinding.Component", {
-		metadata: {
-			interfaces: ["sap.ui.core.IAsyncContentCreation"],
-			manifest: "json"
-		}
-	});
+    return UIComponent.extend("ui5.databinding.Component", {
+        metadata: {
+            interfaces: ["sap.ui.core.IAsyncContentCreation"],
+            manifest: "json"
+        },
+
+        init: function () {
+            UIComponent.prototype.init.apply(this, arguments);
+
+            // Cargar y asignar el modelo JSON
+            const oModel = new JSONModel();
+            oModel.loadData("model/data.json");
+            this.setModel(oModel);
+        }
+    });
 });
